7bb4da007b02fed2e63d60467f962e8a618e4ffe
RohitGiriRg <rohitgirirg7027@gmail.com>
2026-01-21 23:24:24 +0530

    in

---
+++ app/Homepage/Homepage.tsx
@@ -0,0 +1,418 @@
+"use client"
+
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <>
+            <div className="home-container10">
+                <div className="home-container11">
+                    <div className="home-container12">
+                        <div className="home-container13">
+                            <h1>RgLinkly</h1>
+                        </div>
+                        <div className="home-container14">
+                            <button type="button" className="button">
+                                Login 👤
+                            </button>
+                            <button type="button" className="button">
+                                Register Now
+                            </button>
+                        </div>
+                    </div>
+                    <div className="home-container15">
+                        <h1>Shorten Your Loooong Links :)</h1>
+                        <span>
+                            Linkly is an efficient and easy-to-use URL shortening service that
+                            streamlines your online experience.
+                        </span>
+                        <div className="home-container16">
+                            <div className="home-container17">
+                                <span>🔗</span>
+                                <span>Enter the link nere</span>
+                            </div>
+                            <div className="home-container18">
+                                <button type="button" className="button">
+                                    Shorten Now!
+                                </button>
+                            </div>
+                        </div>
+                        <div className="home-container19">
+                            <input type="radio" name="radio" />
+                            <span>Auto Pasta from Clipboard</span>
+                        </div>
+                        <span>
+                            You can create 05 more links. Register Now to enjoy Unlimited
+                            usage ❔
+                        </span>
+                    </div>
+                    <table className="home-table">
+                        <thead className="home-thead">
+                            <tr>
+                                <th>
+                                    <span>Short Link</span>
+                                </th>
+                                <th>
+                                    <span>Original Link</span>
+                                </th>
+                                <th>
+                                    <span>QR Code</span>
+                                </th>
+                                <th>
+                                    <span>Clicks</span>
+                                </th>
+                                <th>
+                                    <span>Status</span>
+                                </th>
+                                <th>
+                                    <span>Date</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <span>www.h.com</span>
+                                    <button type="button" className="button">
+                                        copy
+                                    </button>
+                                </td>
+                                <td className="home-td11">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image1"
+                                    />
+                                    <span>www.org.com</span>
+                                </td>
+                                <td className="home-td12">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image2"
+                                    />
+                                </td>
+                                <td>
+                                    <span>123</span>
+                                </td>
+                                <td>
+                                    <span>Active</span>
+                                </td>
+                                <td>
+                                    <span>Oct-2-2025</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>www.h.com</span>
+                                    <button type="button" className="button">
+                                        copy
+                                    </button>
+                                </td>
+                                <td className="home-td17">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image3"
+                                    />
+                                    <span>www.org.com</span>
+                                </td>
+                                <td className="home-td18">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image4"
+                                    />
+                                </td>
+                                <td>
+                                    <span>123</span>
+                                </td>
+                                <td>
+                                    <span>Active</span>
+                                </td>
+                                <td>
+                                    <span>Oct-2-2025</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>www.h.com</span>
+                                    <button type="button" className="button">
+                                        copy
+                                    </button>
+                                </td>
+                                <td className="home-td23">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image5"
+                                    />
+                                    <span>www.org.com</span>
+                                </td>
+                                <td className="home-td24">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image6"
+                                    />
+                                </td>
+                                <td>
+                                    <span>123</span>
+                                </td>
+                                <td>
+                                    <span>Active</span>
+                                </td>
+                                <td>
+                                    <span>Oct-2-2025</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <span>www.h.com</span>
+                                    <button type="button" className="button">
+                                        copy
+                                    </button>
+                                </td>
+                                <td className="home-td29">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image7"
+                                    />
+                                    <span>www.org.com</span>
+                                </td>
+                                <td className="home-td30">
+                                    <img
+                                        alt="image"
+                                        src="https://play.teleporthq.io/static/svg/default-img.svg"
+                                        className="home-image8"
+                                    />
+                                </td>
+                                <td>
+                                    <span>123</span>
+                                </td>
+                                <td>
+                                    <span>Active</span>
+                                </td>
+                                <td>
+                                    <span>Oct-2-2025</span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <style jsx>
+                {`
+          .home-container10 {
+            width: 100%;
+            display: flex;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+            justify-content: flex-start;
+          }
+          .home-container11 {
+            width: 100%;
+            height: 100%;
+            display: block;
+            position: relative;
+            max-width: 100%;
+            min-width: 100%;
+            align-self: center;
+            box-shadow: 5px 5px 10px 0px #d4d4d4;
+            max-height: 100%;
+            min-height: 100%;
+            border-color: var(--dl-color-theme-neutral-dark);
+            border-width: 1px;
+            border-radius: var(--dl-layout-radius-radius4);
+          }
+          .home-container12 {
+            flex: 0 0 auto;
+            width: var(--dl-layout-size-xsmall);
+            height: 100%;
+            display: flex;
+            max-width: 100%;
+            min-width: 100%;
+            align-self: center;
+            max-height: 100%;
+            min-height: 100%;
+            align-items: center;
+            justify-self: stretch;
+            justify-content: space-between;
+          }
+          .home-container13 {
+            flex: 0 0 auto;
+            display: flex;
+            align-items: flex-start;
+            justify-content: flex-start;
+          }
+          .home-container14 {
+            flex: 0 0 auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .home-container15 {
+            flex: 0 0 auto;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .home-container16 {
+            flex: 0 0 auto;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+          }
+          .home-container17 {
+            flex: 0 0 auto;
+            width: var(--dl-layout-size-large);
+            height: 100px;
+            display: flex;
+            align-items: flex-start;
+          }
+          .home-container18 {
+            flex: 0 0 auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .home-container19 {
+            flex: 0 0 auto;
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+            justify-content: flex-start;
+          }
+          .home-table {
+            width: 100%;
+            height: 100%;
+            display: table;
+            border-collapse: collapse;
+          }
+          .home-thead {
+            width: 100%;
+            height: 100%;
+          }
+          .home-td11 {
+            display: flex;
+            align-items: center;
+          }
+          .home-image1 {
+            width: 37px;
+            height: 34px;
+            object-fit: cover;
+          }
+          .home-td12 {
+            width: 84px;
+            height: 29px;
+          }
+          .home-image2 {
+            width: 30px;
+            height: 40px;
+            display: flex;
+            object-fit: cover;
+            justify-content: center;
+          }
+          .home-td17 {
+            display: flex;
+            align-items: center;
+          }
+          .home-image3 {
+            width: 37px;
+            height: 34px;
+            object-fit: cover;
+          }
+          .home-td18 {
+            width: 84px;
+            height: 29px;
+          }
+          .home-image4 {
+            width: 30px;
+            height: 40px;
+            display: flex;
+            object-fit: cover;
+            justify-content: center;
+          }
+          .home-td23 {
+            display: flex;
+            align-items: center;
+          }
+          .home-image5 {
+            width: 37px;
+            height: 34px;
+            object-fit: cover;
+          }
+          .home-td24 {
+            width: 84px;
+            height: 29px;
+          }
+          .home-image6 {
+            width: 30px;
+            height: 40px;
+            display: flex;
+            object-fit: cover;
+            justify-content: center;
+          }
+          .home-td29 {
+            display: flex;
+            align-items: center;
+          }
+          .home-image7 {
+            width: 37px;
+            height: 34px;
+            object-fit: cover;
+          }
+          .home-td30 {
+            width: 84px;
+            height: 29px;
+          }
+          .home-image8 {
+            width: 30px;
+            height: 40px;
+            display: flex;
+            object-fit: cover;
+            justify-content: center;
+          }
+          .home-link {
+            display: contents;
+          }
+          .home-container20 {
+            right: 50px;
+            border: 1px solid #ffffff5c;
+            bottom: 30px;
+            display: flex;
+            z-index: 22;
+            position: fixed;
+            box-shadow: 5px 5px 10px 0px rgba(31, 31, 31, 0.4);
+            min-height: auto;
+            align-items: center;
+            padding-top: 8px;
+            padding-left: 12px;
+            border-radius: 8px;
+            padding-right: 12px;
+            padding-bottom: 8px;
+            backdrop-filter: blur(6px);
+            background-color: rgba(41, 41, 41, 0.41);
+          }
+          .home-icon1 {
+            width: 24px;
+            margin-right: 4px;
+          }
+          .home-text43 {
+            color: white;
+            font-size: 13px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: 24px;
+          }
+        `}
+            </style>
+        </>
+    )
+}
+
+export default Homepage;
